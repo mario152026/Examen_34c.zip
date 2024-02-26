c103c99da8841d8f8defea1b32bbe6f46ecbba52
MarioJimenez <daw15.2026@gmail.com>
2024-02-26 13:33:04 +0100

    "Proyeto sin documentar"

---
+++ src/Examen/RockpaperScissorsTest.java
@@ -0,0 +1,14 @@
+package Examen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RockpaperScissorsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
